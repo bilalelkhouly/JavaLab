af24b8cad833b16d8fa0eca9469708023bbf736a
Bilal <Bilal@10.0.0.61>
2022-01-25 13:57:02 -0500

    Initial commit

--- src/javalab/JavaLab.java
+++ src/javalab/JavaLab.java
@@ -14,7 +14,7 @@ public class JavaLab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Welcome to git programming");
+        System.out.println("Welcome to git ");
     }
     
 }
