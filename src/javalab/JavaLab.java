b208eb58896d282374fceefb2e125415651ea95c
Bilal <Bilal@10.0.0.61>
2022-01-25 13:55:40 -0500

    Initial commit

---
+++ src/javalab/JavaLab.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javalab;
+
+/**
+ *
+ * @author Bilal
+ */
+public class JavaLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to git programming");
+    }
+    
+}
